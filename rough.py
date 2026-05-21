ecb48708fd973ee6c739f1fb26abd41e36a47354
PrachiUkey <prachi.ukey@flamapp.com>
2026-05-21 14:55:55 +0530

    sanity check

--- rough.py
+++ rough.py
@@ -4,3 +4,18 @@
 # 472 images, 472 masks
 # 60 det images  COCO annotations
 # 389 cls images across 5 folders
+
+from PIL import Image
+import numpy as np, json
+import cv2
+
+img= cv2.imread("dataset/segmentation/images/image_1734186212166.jpg")
+mask=cv2.imread("dataset/segmentation/masks/image_1734186212166.png")
+
+print("==========================")
+print(img.shape, mask.shape)
+print("==========================")
+print(img.dtype, mask.dtype)
+print("==========================")
+print(np.unique(img),np.unique(mask))
+print("==========================")
